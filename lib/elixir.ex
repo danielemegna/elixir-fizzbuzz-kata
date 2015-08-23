8b66719115a7f4d71828bcaaf1a51f48a8a811f8
Daniele Megna <megna.dany@gmail.com>
2015-08-23 09:51:48 +0200

    Fizz test passed

--- lib/elixir.ex
+++ lib/elixir.ex
@@ -1,5 +1,9 @@
 defmodule FizzBuzz do
-  def work n do
+  def work(n) when n == 3 do
+    "Fizz"
+  end
+
+  def work(n) do
     to_string n 
   end
 end
--- test/elixir_test.exs
+++ test/elixir_test.exs
@@ -4,4 +4,8 @@ defmodule FizzBuzzTest do
   test "number to string" do
     assert FizzBuzz.work(1) == "1"
   end
+  
+  test "multiples of 3 are fizz" do
+    assert FizzBuzz.work(3) == "Fizz"
+  end
 end
