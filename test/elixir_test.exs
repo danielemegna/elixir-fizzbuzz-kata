6640a6a084ff9c99d3097bcb065d6c7a6dda33c9
Daniele Megna <megna.dany@gmail.com>
2015-08-23 10:06:33 +0200

    Removed duplication into tests

--- test/elixir_test.exs
+++ test/elixir_test.exs
@@ -2,14 +2,18 @@ defmodule FizzBuzzTest do
   use ExUnit.Case
 
   test "number to string" do
-    assert FizzBuzz.work(1) == "1"
+    assertFizzBuzz 1, "1"
   end
   
   test "multiples of 3 are fizz" do
-    assert FizzBuzz.work(9) == "Fizz"
+    assertFizzBuzz 9, "Fizz"
   end
 
   test "multiples of 5 are buzz" do
-    assert FizzBuzz.work(10) == "Buzz"
+    assertFizzBuzz 10, "Buzz"
+  end
+
+  defp assertFizzBuzz n, expected do
+    assert FizzBuzz.work(n) == expected
   end
 end
