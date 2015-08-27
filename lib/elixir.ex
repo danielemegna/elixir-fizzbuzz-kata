48f4e08dddc24412a6155462c2543b1b9569efbb
Daniele Megna <megna.dany@gmail.com>
2015-08-27 20:45:33 +0200

    Extracted is_akin? method

--- lib/elixir.ex
+++ lib/elixir.ex
@@ -8,10 +8,14 @@ defmodule FizzBuzz do
   def work(n), do: work(n, @dictionary, "")
 
   defp work(n, [ {divisor, word} | tail ], result) do
-    if rem(n, divisor) == 0, do: result = result <> word
+    if is_akin?(n, divisor), do: result = result <> word
     work(n, tail, result)
   end
 
   defp work(n, [], ""), do: to_string(n)
   defp work(_, [], result), do: result
+
+  defp is_akin?(n, divisor) when rem(n, divisor) == 0, do: true
+  defp is_akin?(n, divisor), do: false
+
 end
