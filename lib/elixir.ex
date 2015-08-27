c5898eb13ecd5c43565b57305679b5c2a71ffd16
Daniele Megna <megna.dany@gmail.com>
2015-08-27 20:48:14 +0200

    Improved names

--- lib/elixir.ex
+++ lib/elixir.ex
@@ -7,15 +7,15 @@ defmodule FizzBuzz do
 
   def work(n), do: work(n, @dictionary, "")
 
-  defp work(n, [ {divisor, word} | tail ], result) do
-    if is_akin?(n, divisor), do: result = result <> word
-    work(n, tail, result)
+  defp work(n, [ {divisor, word} | tail ], partial_result) do
+    if is_akin?(n, divisor), do: partial_result = partial_result <> word
+    work(n, tail, partial_result)
   end
 
   defp work(n, [], ""), do: to_string(n)
-  defp work(_, [], result), do: result
+  defp work(_, [], final_result), do: final_result
 
   defp is_akin?(n, divisor) when rem(n, divisor) == 0, do: true
-  defp is_akin?(n, divisor), do: false
+  defp is_akin?(_, _), do: false
 
 end
